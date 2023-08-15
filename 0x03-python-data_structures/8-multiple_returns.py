0f07499c29422acdc7d44c2fd34970265fa5b126
ARA-01 <olajideeniola75@gmail.com>
2023-08-15 16:16:07 -0700

    ...

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-    if len(sentence) == "":
+    if sentence == "":
         first_char = None
     else:
         first_char = sentence[0]
-    
+
     return (len(sentence), first_char)
