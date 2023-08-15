b12bb4d3d6425b31b27880c832189defaea280fa
ARA-01 <olajideeniola75@gmail.com>
2023-08-15 16:39:48 -0700

    ...

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -4,9 +4,9 @@ def max_integer(my_list=[]):
     if len(my_list) == 0:
         return (None)
 
-    large = my_list[0]
+    big = my_list[0]
     for i in range(len(my_list)):
-        if my_list[i] > large:
-            large = my_list[i]
+        if my_list[i] > big:
+            big = my_list[i]
 
-    return (large)
+    return (big)
