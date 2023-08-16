14d99ff843022d688ed46354dc827b12b9c40439
ARA-01 <olajideeniola75@gmail.com>
2023-08-16 16:15:20 -0700

    0x02. Python - import & modules

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,27 +1,19 @@
 #!/usr/bin/python3
 
-import sys
-from calculator_1 import add, sub, mul, div
-
-# Define a dictionary that maps operators to their corresponding functions
-operations = {
-    "+": add,
-    "-": sub,
-    "*": mul,
-    "/": div
-}
-
 if __name__ == "__main__":
     """Handle basic arithmetic operations."""
-    if len(sys.argv) != 4:
+    from calculator_1 import add, sub, mul, div
+    import sys
+
+    if len(sys.argv) - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
-    a, operator, b = int(sys.argv[1]), sys.argv[2], int(sys.argv[3])
-
-    if operator not in operations:
-        print("Unknown operator. Available operators:", ", ".join(operations.keys()))
+    ops = {"+": add, "-": sub, "*": mul, "/": div}
+    if sys.argv[2] not in list(ops.keys()):
+        print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
 
-    result = operations[operator](a, b)
-    print(f"{a} {operator} {b} = {result}")
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
