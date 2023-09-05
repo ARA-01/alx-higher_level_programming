3380e944cff5de0e5521e6068c7c58480d62b182
ARA-01 <olajideeniola75@gmail.com>
2023-09-04 18:36:44 -0700

    Python - More Classes and Objects

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -60,5 +60,5 @@ class Rectangle:
         for i in range(self.__height):
             [rectangle_str.append('#') for j in range(self.__width)]
             if i != self.__height - 1:
-                rectangle_str.append("\n")         
+                rectangle_str.append("\n")
         return ("".join(rectangle_str))
