8653bde44758847f26acd6ae69618bd9ea367d43
ARA-01 <olajideeniola75@gmail.com>
2023-11-15 07:26:57 -0800

    0x12. JavaScript - Warm up

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
 // Use const to create constants
-const firstLine = "C is fun";
-const secondLine = "Python is cool";
-const thirdLine = "JavaScript is amazing";
+const firstLine = 'C is fun';
+const secondLine = 'Python is cool';
+const thirdLine = 'JavaScript is amazing';
 
 // Print each lne using console.log()
 console.log(firstLine);
